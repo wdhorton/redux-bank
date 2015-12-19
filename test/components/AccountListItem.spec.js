5eee00448262a807692263ebee867f59aecff4ff
wdhorton <wdt.horton@gmail.com>
2015-12-19 10:41:36 -0500

    Test if AccountListItem renders account number from props.

--- test/components/AccountListItem.spec.js
+++ test/components/AccountListItem.spec.js
@@ -4,11 +4,16 @@ import TestUtils from 'react-addons-test-utils';
 import AccountListItem from '../../components/AccountListItem.js';
 
 function setup() {
+  const props = {
+    accountNumber: "123456789"
+  };
+
   const renderer = TestUtils.createRenderer();
-  renderer.render(<AccountListItem />);
+  renderer.render(<AccountListItem {...props} />);
   const output = renderer.getRenderOutput();
 
   return {
+    props: props,
     output: output,
     renderer: renderer
   };
@@ -16,10 +21,19 @@ function setup() {
 
 describe("components", () => {
   describe("AccountListItem", () => {
-    it("initial render", () => {
+    it("renders as an li", () => {
       const { output } = setup();
 
       expect(output.type).toBe('li');
     });
+
+    it("displays the account number passed as props", () => {
+      const { output, props } = setup();
+
+      const div = output.props.children;
+
+      expect(div.innerHTML).toBe(props.accountNumber);
+    });
+
   });
 });
