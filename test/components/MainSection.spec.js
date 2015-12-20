7d78ddcd6c358e8aa291f7742042f1149967bdc9
wdhorton <wdt.horton@gmail.com>
2015-12-20 11:10:36 -0500

    Write first test for MainSection.

---
+++ test/components/MainSection.spec.js
@@ -0,0 +1,24 @@
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+
+function setup() {
+  const renderer = TestUtils.createRenderer();
+  renderer.render(<MainSection />);
+  const output = renderer.getRenderOutput();
+
+  return {
+    output,
+    renderer
+  };
+}
+
+describe("components", () => {
+  describe("MainSection", () => {
+    it("renders container", () => {
+      const { output } = setup();
+
+      expect(output.type).toBe('section');
+    });
+  });
+});
