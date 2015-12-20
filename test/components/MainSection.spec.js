ebe6f4dd0e436183f60391f31feb2a28dc5f9c47
wdhorton <wdt.horton@gmail.com>
2015-12-20 11:12:48 -0500

    Write render function for MainSection component.

--- test/components/MainSection.spec.js
+++ test/components/MainSection.spec.js
@@ -1,6 +1,7 @@
 import expect from 'expect';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
+import MainSection from '../../components/MainSection.js';
 
 function setup() {
   const renderer = TestUtils.createRenderer();
