f385551eaa9a2d4b0327950b54260938d47f84d5
wdhorton <wdt.horton@gmail.com>
2015-12-21 11:53:41 -0500

    Create Transaction component.

--- test/components/account/Transaction.spec.js
+++ test/components/account/Transaction.spec.js
@@ -1,6 +1,7 @@
 import expect from 'expect';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
+import Transaction from '../../../components/account/Transaction';
 
 function setup() {
   const props = {};
